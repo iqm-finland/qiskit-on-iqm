a34fff5ffae09e9ebf19bc5a18a20f871a572400
Leon Wubben <leon@meetiqm.com>
2022-02-11 18:10:03 +0200

    Add result implementation for IQMJob

--- src/qiskit_iqm_provider/iqm_job.py
+++ src/qiskit_iqm_provider/iqm_job.py
@@ -11,10 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import uuid
+from datetime import date
 from typing import List
 
-from iqm_client.iqm_client import IQMClient, Circuit, SingleQubitMapping
+from iqm_client.iqm_client import IQMClient, Circuit, SingleQubitMapping, RunStatus
 from qiskit.providers import JobV1 as Job, JobStatus
+from qiskit.result import Result
+from qiskit.result.models import ExperimentResultData
 from qiskit_iqm_provider import IQMBackend
 
 
@@ -29,6 +33,9 @@ class IQMJob(Job):
         self.circuit_json = circuit_json
         self.mapping_json = mapping_json
         self.shots = shots
+        self._status = None
+        self._client = IQMClient(backend.url, backend.settings)
+        self._job_id = None
         if 'job_id' in kwargs:
             job_id = kwargs['job_id']
             del kwargs['job_id']
@@ -43,20 +50,34 @@ class IQMJob(Job):
         return self._backend
 
     def submit(self):
-        client = IQMClient(self.backend().url, self.backend().settings)
-        job_id = client.submit_circuit(self.circuit_json, qubit_mapping=self.mapping_json, shots=self.shots)
-        self._job_id = str(job_id)
+        job_id = self._client.submit_circuit(self.circuit_json, qubit_mapping=self.mapping_json, shots=self.shots)
+        self._job_id = job_id.hex
+        self._status = JobStatus.RUNNING
 
-    def result(self):
+    def result(self) -> Result:
+        if self.status() != JobStatus.RUNNING and self.status() != JobStatus.DONE:
+            raise RuntimeError  # TODO custom Exception
         # Rough sketch of implementation
         # 1. get result from iqm client
+        result = self._client.wait_for_results(uuid.UUID(self._job_id))
+        self._status = JobStatus.DONE
         # 2. extract measurement results: a=result.measurements['mk']
         # 3. Construct Qiskit ExperimentResultData object with b=ExperimentResultData(memory=a)
+        qiskit_results = ExperimentResultData(memory=result.measurements['mk'])
         # 4. return Result([b])
-        raise NotImplementedError
+        return Result(
+            backend_name=self.backend().name,
+            backend_version=self.backend().backend_version,
+            qobj_id=None,
+            job_id=self._job_id,
+            success=True,
+            results=[qiskit_results],
+            date=date.today(),
+        )
 
     def cancel(self):
-        raise NotImplementedError
+        pass
 
     def status(self) -> JobStatus:
-        raise NotImplementedError
+        return self._status
+
