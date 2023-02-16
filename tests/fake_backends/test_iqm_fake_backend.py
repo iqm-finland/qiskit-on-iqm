54fe1d6cb801e86fc59d9a2effb26dee4a2dbd64
Stefan Seegerer <stefan.seegerer@meetiqm.com>
2023-02-16 16:45:31 +0100

    clean up tests

--- tests/fake_backends/test_iqm_fake_backend.py
+++ tests/fake_backends/test_iqm_fake_backend.py
@@ -13,13 +13,9 @@
 # limitations under the License.
 """Testing IQM fake backend.
 """
-import numpy as np
 import pytest
 from qiskit import QuantumCircuit
-from qiskit.circuit import Parameter
-from qiskit.compiler import transpile
 from qiskit.providers import JobV1
-from qiskit_aer import noise
 from qiskit_aer.noise.noise_model import NoiseModel
 
 from qiskit_iqm import IQMFakeBackend
@@ -30,7 +26,19 @@ def backend(create_chip_sample):
     return IQMFakeBackend(create_chip_sample())
 
 
+def test_iqm_fake_backend_noise_model_instantiated(backend):
+    """Test that creating a Fake Backend instantiates a Qiskit noise model"""
+    assert isinstance(backend.noise_model, NoiseModel)
+
+
+def test_iqm_fake_backend_noise_model_basis_gates(backend):
+    """Test that all operations named as part of the backend are utilizes in the noise_model"""
+    assert all(gates in backend.operation_names for gates in backend.noise_model.basis_gates)
+
+
 def test_run_single_circuit(backend):
+    """Test that the backend can be called with a circuit
+    or a list of circuits and returns a result."""
     circuit = QuantumCircuit(1, 1)
     circuit.measure(0, 0)
     shots = 10
@@ -44,43 +52,10 @@ def test_run_single_circuit(backend):
     assert job.result() is not None
 
 
-def test_run_with_non_default_settings(backend):
-    circuit = QuantumCircuit(1, 1)
-    circuit.measure(0, 0)
-    shots = 10
-    settings = {"setting1": 5}
-
-    job = backend.run([circuit], qubit_mapping=None, settings=settings, shots=shots)
-    assert isinstance(job, JobV1)
-    assert job.result() is not None
-
-
-def test_run_batch_of_circuits(backend):
-    qc = QuantumCircuit(2)
-    theta = Parameter("theta")
-    theta_range = np.linspace(0, 2 * np.pi, 3)
-    shots = 10
-    qc.cz(0, 1)
-    qc.r(theta, 0, 0)
-    qc.cz(0, 1)
-    circuits = [qc.bind_parameters({theta: t}) for t in theta_range]
-
-    job = backend.run(circuits, qubit_mapping={qc.qubits[0]: "QB1", qc.qubits[1]: "QB2"}, shots=shots)
-    assert isinstance(job, JobV1)
-    assert job.result() is not None
-
-
 def test_error_on_empty_circuit_list(backend):
+    """Test that calling run with an empty list of circuits raises a ValueError."""
     with pytest.raises(ValueError, match="Empty list of circuits submitted for execution."):
-        backend.run([], qubit_mapping={}, shots=42)
-
-
-def test_noise_model_is_valid(backend):
-    """
-    Tests if noise model of a fake backend is a Qiskit noise model object.
-    """
-    noise_model = backend.noise_model
-    assert isinstance(noise_model, noise.NoiseModel)
+        backend.run([], qubit_mapping=None)
 
 
 def test_noise_on_all_qubits(backend):
@@ -103,32 +78,3 @@ def test_noise_model_has_noise_terms(backend):
     noise_model = backend.noise_model
 
     assert not noise_model.is_ideal()
-
-
-def test_noisy_bell_state(backend):
-    """
-    Tests if a generated Bell state is noisy.
-    """
-    circ = QuantumCircuit(2)
-    circ.h(0)
-    circ.cx(0, 1)
-    circ.measure_all()
-
-    transpiled_circuit = transpile(circ, backend=backend)
-    job = backend.run(transpiled_circuit, shots=10000)
-    counts = job.result().get_counts()
-
-    wrong_results = 0
-    if "01" in counts:
-        wrong_results += counts["01"]
-    if "10" in counts:
-        wrong_results += counts["10"]
-    assert wrong_results > 0
-
-
-def test_iqm_fake_adonis_noise_model_instantiated(backend):
-    assert isinstance(backend.noise_model, NoiseModel)
-
-
-def test_iqm_fake_backend_noise_model_basis_gates(backend):
-    assert all(gates in backend.operation_names for gates in backend.noise_model.basis_gates)
