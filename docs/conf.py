d457fdf7fd00e154726e1d8e2aee7f37a3744dad
Ville Bergholm <ville@meetiqm.com>
2023-03-15 22:35:12 +0200

    Update intersphinx mapping.

--- docs/conf.py
+++ docs/conf.py
@@ -152,7 +152,8 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/' + python_version, None),
     'matplotlib': ('https://matplotlib.org/stable', None),
     'numpy': ('https://numpy.org/doc/stable', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy', None),
+    'iqm-client': ('https://iqm-finland.github.io/iqm-client', None),
 }
 
 extlinks = {
