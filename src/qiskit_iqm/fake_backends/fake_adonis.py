3133092798aa0c38e8b582e384d953474ae5bda2
Adrian Auer <adrian.auer@meetiqm.com>
2023-03-20 10:17:01 +0100

    Fixed docstring of fake_adonis.py.

--- src/qiskit_iqm/fake_backends/fake_adonis.py
+++ src/qiskit_iqm/fake_backends/fake_adonis.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Fake (i.e. simulated) backend for IQM Adonis QPU.
+"""Fake backend specific for IQM's 5-qubit Adonis QPU.
 """
 from iqm_client import QuantumArchitectureSpecification
 
