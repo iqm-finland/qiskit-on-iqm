c6a57ea3a51c496c72fbbb1a80e135fcf73177af
Matthias Beuerle <matthias.beuerle@meetiqm.com>
2023-09-26 09:09:50 +0200

    Docs + tests.

--- src/iqm/qiskit_iqm/iqm_transpilation.py
+++ src/iqm/qiskit_iqm/iqm_transpilation.py
@@ -15,23 +15,39 @@
 import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.circuit import Qubit
-from qiskit.circuit.library import RGate, RZGate
+from qiskit.circuit.library import RGate
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.transpiler.basepasses import TransformationPass
 from qiskit.transpiler.passes import Optimize1qGatesDecomposition, Unroller
 from qiskit.transpiler.passmanager import PassManager
 
+# pylint: disable=anomalous-backslash-in-string
 
-class OptimizeVirtualZRotation(TransformationPass):
+
+class IQMOptimize1QbDecomposition(TransformationPass):
+    """Optimize the decomposition of 1 qubit gates for the IQM gate set. This optimisation pass expects
+    the circuit to be correctly layouted and translated to the IQM architecture and raises an error otherwise.
+    The optimisation logic follows the following steps:
+
+    1. Convert single qubit gates to `U` gates and combine all neighbouring `U` gates
+    2. Convert `U` gates according to
+       :math:`U(\\theta , \phi , \lambda) = R_Z(\phi + \lambda) R(\\theta, \pi / 2  - \lambda)`
+    3. Commute `R_Z` gates to the end of the circuit using the fact that `R_Z` and `C_Z` gates commute and
+       :math:`R(\\theta , \phi) R_Z(\lambda) = R_Z(\lambda) R(\\theta, \phi - \lambda)`
+    4. Drop `R_Z` gates immediately before measurements otherwise replace according to
+       :math:`R_Z(\lambda) = R_X(\pi / 2) R_Y(\lambda) R_X(-\pi / 2)`
+    """
+
     def __init__(self):
         super().__init__()
         self._basis = ['r', 'cz']
         self._intermediate_basis = ['u', 'cz']
 
     def run(self, dag: DAGCircuit):
+        self._validate_ops(dag)
         virtual_z_frames: list[float] = [0] * dag.num_qubits()
         optimized_dag: DAGCircuit = Unroller(self._intermediate_basis).run(dag)
-        optimized_dag: DAGCircuit = Optimize1qGatesDecomposition(self._intermediate_basis).run(optimized_dag)
+        optimized_dag = Optimize1qGatesDecomposition(self._intermediate_basis).run(optimized_dag)
         last_op_is_measurement: dict[Qubit, bool] = {qubit: False for qubit in dag.qubits}
         for node in dag.topological_op_nodes():
             if node.name == 'u':
@@ -40,7 +56,7 @@ class OptimizeVirtualZRotation(TransformationPass):
                     node, RGate(node.op.params[0], np.pi / 2 - node.op.params[2] - virtual_z_frames[qubit_index])
                 )
                 virtual_z_frames[qubit_index] += node.op.params[1] + node.op.params[2]
-            if node.name is 'measure':
+            if node.name == 'measure':
                 for qubit in node.qargs:
                     last_op_is_measurement[qubit] = True
             else:
@@ -50,9 +66,9 @@ class OptimizeVirtualZRotation(TransformationPass):
         for qubit, has_terminal_measurement in last_op_is_measurement.items():
             qubit_index = optimized_dag.find_bit(qubit)[0]
             if not has_terminal_measurement and virtual_z_frames[qubit_index] != 0:
-                optimized_dag.apply_operation_back(RGate(np.pi / 2, 0), qargs=(qubit,))
-                optimized_dag.apply_operation_back(RGate(virtual_z_frames[qubit_index], np.pi / 2), qargs=(qubit,))
                 optimized_dag.apply_operation_back(RGate(-np.pi / 2, 0), qargs=(qubit,))
+                optimized_dag.apply_operation_back(RGate(virtual_z_frames[qubit_index], np.pi / 2), qargs=(qubit,))
+                optimized_dag.apply_operation_back(RGate(np.pi / 2, 0), qargs=(qubit,))
 
         return optimized_dag
 
@@ -60,10 +76,18 @@ class OptimizeVirtualZRotation(TransformationPass):
         for node in dag.op_nodes():
             if node.name not in self._basis + ['measure', 'barrier']:
                 raise ValueError(
-                    f"Invalid operation '{node.name}' found in OptimizeVirtualZRotation pass, expected operations {self._basis + ['measurement', 'barrier']}"
+                    f"""Invalid operation '{node.name}' found in IQMOptimize1QbDecomposition pass, 
+                    expected operations {self._basis + ['measurement', 'barrier']}"""
                 )
 
 
-def optimize_virtual_z_rotations(circuit: QuantumCircuit):
-    """Optimize number of single qubit gates in a transpiled circuit exploting the IQM specific gate set."""
-    return PassManager(OptimizeVirtualZRotation()).run(circuit)
+def optimize_1_qb_gate_decomposition(circuit: QuantumCircuit):
+    """Optimize number of single qubit gates in a transpiled circuit exploting the IQM specific gate set.
+
+    Args:
+        circuit: quantum circuit to optimise
+
+    Returns:
+        optimised circuit
+    """
+    return PassManager(IQMOptimize1QbDecomposition()).run(circuit)
---
+++ tests/test_iqm_transpilation.py
@@ -0,0 +1,53 @@
+# Copyright 2022-2023 Qiskit on IQM developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Testing IQM transpilation.
+"""
+import numpy as np
+import pytest
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import Aer
+
+from iqm.qiskit_iqm.iqm_transpilation import optimize_1_qb_gate_decomposition
+
+
+def test_optimize_1qb_gate_decomposition():
+    """Test that 1 qubit gate decomposition preserves the unitary of the circuit."""
+    circuit = QuantumCircuit(2, 2)
+    circuit.t(0)
+    circuit.rx(0.4, 0)
+    circuit.cnot(0, 1)
+    circuit.ry(0.7, 1)
+    circuit.h(1)
+    circuit.r(0.2, 0.8, 0)
+    circuit.h(0)
+
+    simulator = Aer.get_backend(name='unitary_simulator')
+
+    transpiled_circuit = transpile(circuit, basis_gates=['r', 'cz'])
+    optimized_circuit = optimize_1_qb_gate_decomposition(transpiled_circuit)
+
+    transpiled_unitary = simulator.run(transpiled_circuit).result().get_unitary(transpiled_circuit)
+
+    optimized_unitary = simulator.run(optimized_circuit).result().get_unitary(transpiled_circuit)
+
+    np.testing.assert_almost_equal(transpiled_unitary, optimized_unitary)
+
+
+def test_optimize_1qb_gate_decomposition_raises_on_invalid_basis():
+    """Test that optimisation pass raises error if gates other than `R_Z, C_Z` are provided."""
+    circuit = QuantumCircuit(1, 1)
+    circuit.h(0)
+
+    with pytest.raises(ValueError, match="Invalid operation 'h' found "):
+        optimize_1_qb_gate_decomposition(circuit)
