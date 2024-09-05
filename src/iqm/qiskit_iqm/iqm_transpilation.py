1dd4b29a99fa4106e389bfff183a21f0a42ef811
Arianne Meijer <arianne.meijer@meetiqm.com>
2024-09-05 15:36:59 +0300

    removed unused import

--- src/iqm/qiskit_iqm/iqm_transpilation.py
+++ src/iqm/qiskit_iqm/iqm_transpilation.py
@@ -21,7 +21,6 @@ from qiskit.dagcircuit import DAGCircuit
 from qiskit.transpiler.basepasses import TransformationPass
 from qiskit.transpiler.passes import BasisTranslator, Optimize1qGatesDecomposition, RemoveBarriers
 from qiskit.transpiler.passmanager import PassManager
-from qiskit.transpiler.preset_passmanagers.plugin import PassManagerStagePlugin
 
 
 class IQMOptimizeSingleQubitGates(TransformationPass):
