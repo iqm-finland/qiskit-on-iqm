64642a02ac359a139fcbcfe5318dc698dbb50e7a
Arianne Meijer <arianne.meijer@meetiqm.com>
2025-02-18 12:45:16 +0200

    Removed qiskit-monitor tests as this will always be skipped

--- tests/test_iqm_job.py
+++ tests/test_iqm_job.py
@@ -258,28 +258,3 @@ def test_result_with_timestamps(job, iqm_result_two_registers, iqm_metadata_with
     assert job.metadata.pop('timestamps') == iqm_metadata_with_timestamps.get('timestamps')
     assert 'timestamps' in result._metadata
     assert result.timestamps == iqm_metadata_with_timestamps.get('timestamps')
-
-
-def test_job_monitor(job, iqm_metadata):
-    # pylint: disable=import-error
-    # pylint: disable=import-outside-toplevel
-    # pylint: disable=no-name-in-module
-    if qiskit.__version__ >= '1.0.0':
-        pytest.skip('Job monitoring support has been removed from Qiskit v1.0.0 onwards.')
-    else:
-        from qiskit.tools.monitor import job_monitor
-
-        client_result = RunResult(status=Status.READY, metadata=iqm_metadata)
-        run_responses = [
-            [RunStatus(status=Status.PENDING_COMPILATION), RunStatus(status=Status.ABORTED)],
-            [RunStatus(status=Status.PENDING_COMPILATION)] * 2 + [RunStatus(status=Status.FAILED)],
-            [RunStatus(status=Status.PENDING_COMPILATION)] * 2
-            + [RunStatus(status=Status.PENDING_EXECUTION), client_result],
-            [RunStatus(status=s.value) for s in Status],
-        ]
-        sep = '---'
-        for responses in run_responses:
-            when(job._client).get_run_status(uuid.UUID(job.job_id())).thenReturn(*responses)
-            monitor_string = io.StringIO()
-            job_monitor(job, output=monitor_string, line_discipline=sep, interval=0)
-            monitor_string.close()
